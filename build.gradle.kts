4ee3ce34521ed112f1a040ec7001d656b52f55a6
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-10-16 16:33:16 +1100

    feat: wip connected textures

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,40 @@
 plugins {
-    `java-library`
-    alias(libs.plugins.moddevgradle)
+	`java-library`
+	alias(libs.plugins.moddevgradle)
 }
 
 class ModInfo {
-    val id = property("mod.id").toString()
-    val group = property("mod.group").toString()
-    val version = property("mod.version").toString()
-    val name = property("mod.name").toString()
-    val description = property("mod.description").toString()
-    val authors = property("mod.authors").toString()
-    val license = property("mod.license").toString()
+	val id = property("mod.id").toString()
+	val group = property("mod.group").toString()
+	val version = property("mod.version").toString()
+	val name = property("mod.name").toString()
+	val description = property("mod.description").toString()
+	val authors = property("mod.authors").toString()
+	val license = property("mod.license").toString()
 }
 
 val mod = ModInfo()
 
 val generateModMetadata = tasks.register<ProcessResources>("generateModMetadata") {
-    val replaceProperties = mapOf(
-        "minecraft_version" to libs.versions.minecraft.asProvider().get(),
-        "minecraft_version_range" to libs.versions.minecraft.range.get(),
-        "neo_version" to libs.versions.neoforge.asProvider().get(),
-        "neo_version_range" to libs.versions.neoforge.range.get(),
-        "loader_version_range" to libs.versions.loader.range.get(),
-        "mod_id" to mod.id,
-        "mod_name" to mod.name,
-        "mod_license" to mod.license,
-        "mod_version" to mod.version,
-        "mod_authors" to mod.authors,
-        "mod_description" to mod.description
-    )
+	val replaceProperties = mapOf(
+		"minecraft_version" to libs.versions.minecraft.asProvider().get(),
+		"minecraft_version_range" to libs.versions.minecraft.range.get(),
+		"neo_version" to libs.versions.neoforge.asProvider().get(),
+		"neo_version_range" to libs.versions.neoforge.range.get(),
+		"loader_version_range" to libs.versions.loader.range.get(),
+		"mod_id" to mod.id,
+		"mod_name" to mod.name,
+		"mod_license" to mod.license,
+		"mod_version" to mod.version,
+		"mod_authors" to mod.authors,
+		"mod_description" to mod.description
+	)
 
-    inputs.properties(replaceProperties)
-    expand(replaceProperties)
+	inputs.properties(replaceProperties)
+	expand(replaceProperties)
 
-    from("src/main/templates")
-    into(layout.buildDirectory.dir("generated/sources/modMetadata"))
+	from("src/main/templates")
+	into(layout.buildDirectory.dir("generated/sources/modMetadata"))
 }
 
 
@@ -45,84 +45,91 @@ group = mod.group
 base.archivesName.set(mod.id)
 
 repositories {
-    mavenLocal()
+	repositories {
+		exclusiveContent {
+			forRepository { maven("https://api.modrinth.com/maven") }
+			filter { includeGroup("maven.modrinth") }
+		}
+	}
+
+	mavenLocal()
 }
 
 
 java.toolchain.languageVersion = JavaLanguageVersion.of(21)
 
 neoForge {
-    version = libs.versions.neoforge.asProvider().get()
+	version = libs.versions.neoforge.asProvider().get()
 
-    parchment {
-        mappingsVersion = libs.versions.parchment.get()
-        minecraftVersion = libs.versions.minecraft.asProvider().get()
-    }
+	parchment {
+		mappingsVersion = libs.versions.parchment.get()
+		minecraftVersion = libs.versions.minecraft.asProvider().get()
+	}
 
-    runs {
-        register("client") {
-            client()
+	runs {
+		register("client") {
+			client()
 
-            systemProperty("neoforge.enabledGameTestNamespaces", mod.id)
-        }
+			systemProperty("neoforge.enabledGameTestNamespaces", mod.id)
+		}
 
-        register("server") {
-            server()
-            programArgument("--nogui")
-            systemProperty("neoforge.enabledGameTestNamespaces", mod.id)
-        }
+		register("server") {
+			server()
+			programArgument("--nogui")
+			systemProperty("neoforge.enabledGameTestNamespaces", mod.id)
+		}
 
-        register("gameTestServer") {
-            type = "gameTestServer"
-            systemProperty("neoforge.enabledGameTestNamespaces", mod.id)
-        }
+		register("gameTestServer") {
+			type = "gameTestServer"
+			systemProperty("neoforge.enabledGameTestNamespaces", mod.id)
+		}
 
-        register("data") {
-            data()
+		register("data") {
+			data()
 
-            programArguments.addAll(
-                "--mod",
-                mod.id,
-                "--all",
-                "--output",
-                file("src/generated/resources/").absolutePath,
-                "--existing",
-                file("src/main/resources/").absolutePath
-            )
-        }
+			programArguments.addAll(
+				"--mod",
+				mod.id,
+				"--all",
+				"--output",
+				file("src/generated/resources/").absolutePath,
+				"--existing",
+				file("src/main/resources/").absolutePath
+			)
+		}
 
-        configureEach {
-            systemProperty("forge.logging.markers", "REGISTRIES")
-            logLevel = org.slf4j.event.Level.DEBUG
-        }
-    }
+		configureEach {
+			systemProperty("forge.logging.markers", "REGISTRIES")
+			logLevel = org.slf4j.event.Level.DEBUG
+		}
+	}
 
-    mods {
-        create(mod.id) {
-            sourceSet(sourceSets.main.get())
-        }
-    }
+	mods {
+		create(mod.id) {
+			sourceSet(sourceSets.main.get())
+		}
+	}
 
-    ideSyncTask(generateModMetadata)
+	ideSyncTask(generateModMetadata)
 }
 
 sourceSets.main.get().resources.srcDir("src/generated/resources")
 
 dependencies {
-
+	implementation(libs.fusion)
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
+	options.encoding = "UTF-8"
 }
 
 tasks.named("neoForgeIdeSync") {
-    dependsOn(generateModMetadata)
+	dependsOn(generateModMetadata)
 }
 
 idea {
-    module {
-        isDownloadSources = true
-        isDownloadJavadoc = true
-    }
+	module {
+		isDownloadSources = true
+		isDownloadJavadoc = true
+	}
 }
