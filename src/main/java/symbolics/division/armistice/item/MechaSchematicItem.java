c8220d3f2e86fad940a6545a675476197810f774
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-11-04 23:17:26 +1100

    fix crash

--- src/main/java/symbolics/division/armistice/item/MechaSchematicItem.java
+++ src/main/java/symbolics/division/armistice/item/MechaSchematicItem.java
@@ -3,8 +3,8 @@ package symbolics.division.armistice.item;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.context.UseOnContext;
 import org.jetbrains.annotations.NotNull;
+import symbolics.division.armistice.component.SkinComponent;
 import symbolics.division.armistice.mecha.MechaEntity;
-import symbolics.division.armistice.mecha.MechaSkin;
 import symbolics.division.armistice.mecha.schematic.MechaSchematic;
 import symbolics.division.armistice.registry.ArmisticeDataComponentTypeRegistrar;
 import symbolics.division.armistice.registry.ArmisticeEntityTypeRegistrar;
@@ -21,9 +21,9 @@ public class MechaSchematicItem extends ComponentTooltipItem {
 	public InteractionResult useOn(@NotNull UseOnContext context) {
 		MechaSchematic schematic = Optional.ofNullable(context.getItemInHand().get(ArmisticeDataComponentTypeRegistrar.MECHA_SCHEMATIC)).orElseThrow().schematic();
 
-		MechaSkin skin = context.getItemInHand().get(ArmisticeDataComponentTypeRegistrar.SKIN).skin();
+		SkinComponent skin = context.getItemInHand().get(ArmisticeDataComponentTypeRegistrar.SKIN);
 
-		MechaEntity entity = new MechaEntity(ArmisticeEntityTypeRegistrar.MECHA, context.getLevel(), schematic, skin);
+		MechaEntity entity = new MechaEntity(ArmisticeEntityTypeRegistrar.MECHA, context.getLevel(), schematic, Optional.ofNullable(skin).map(SkinComponent::skin).orElse(null));
 		entity.setPos(context.getClickLocation().add(0, 20, 0));
 		context.getLevel().addFreshEntity(entity);
 
